bdc45018d97ad935a9dc5eb7ba72a596125318ba
bigb579 <50326193+bigb579@users.noreply.github.com>
2025-08-27 11:15:16 +0530

    Update provider.tf

--- jenkins/provider.tf
+++ jenkins/provider.tf
@@ -1,3 +1,5 @@
-provider "aws" {
-  region = "us-east-1"
-}
+    provider "aws" {
+      region     = "us-east-1"
+      access_key = "AKIAYS2NU7RAQENMPKMV"
+      secret_key = "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"
+    }

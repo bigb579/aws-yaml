646212261ae7f4ffb27081dd398b270237cd27a1
bigb579 <50326193+bigb579@users.noreply.github.com>
2025-08-27 12:50:01 +0530

    Create variables.tf

---
+++ ec2/variables.tf
@@ -0,0 +1,6 @@
+variable "instance_count" { type = number }
+variable "ami_id" { type = string }
+variable "instance_type" { type = string }
+variable "key_name" { type = string }
+variable "subnet_id" { type = string }
+variable "project" { type = string }
